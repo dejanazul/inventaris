61ea8f3ed9f181e090e3bd2b2bd6fcb3cc8326a6
dejanazoel <dejanazoel@gmail.com>
2025-01-05 21:52:14 +0700

    server fix 001

--- backend/server.js
+++ backend/server.js
@@ -8,14 +8,18 @@ const app = express()
 
 // middleware
 app.use(express.json())
-app.use((req, res) => {
-    res.status(404).json({ error: "Route not found!" })
+
+// routes utama
+app.get("/", (req, res) => {
+    res.send("Welcome to Inventaris API!")
 })
 
 // api routes
 app.use("/barang", itemRoutes)
-app.get("/", (req, res) => {
-    res.send("Welcome to Inventaris API!")
+
+// middleware 404 handler
+app.use((req, res) => {
+    res.status(404).json({ error: "Route not found!" })
 })
 
 const PORT = process.env.PORT || 5000;
