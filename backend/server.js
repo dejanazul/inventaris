3b671c5850472c230c80b9e0273aac029277e229
dejanazoel <dejanazoel@gmail.com>
2025-01-05 18:34:08 +0700

    update api

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,15 @@ const app = express()
 
 // middleware
 app.use(express.json())
+app.use((req, res) => {
+    res.status(404).json({ error: "Route not found!" })
+})
 
 // api routes
 app.use("/barang", itemRoutes)
+app.get("/", (req, res) => {
+    res.send("Welcome to Inventaris API!")
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
