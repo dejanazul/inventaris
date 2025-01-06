8c6a7b4de26bd9edfec45ac7bd6edc478ffc1cfe
dejanazoel <dejanazoel@gmail.com>
2025-01-06 09:29:19 +0700

    server fix 004

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const uri = process.env.DB_URI;
+const uri = process.env.MONGODB_URI;
 
 const client = new MongoClient(uri, {
     serverApi: {
